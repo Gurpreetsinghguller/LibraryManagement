b2255e98f9ef8ae4baaa5ec8ade1a7f2cac437ee
gurpreet <gurpreetsingh.guller@gmail.com>
2022-07-02 23:01:50 +0530

    added comments

--- router/routes.go
+++ router/routes.go
@@ -9,11 +9,15 @@ import (
 func InitializeRoutes(app *controller.Application, e *echo.Echo) {
 	e.GET("/healthcheck", app.HealthCheck)
 
+	// Save book
 	e.POST("/book", app.SaveBook)
+	// Get specific book /book/:Dummy
 	e.GET("/book/:name", app.GetBook)
+	// search for books /book?name=Dummy
 	e.GET("/book", app.ShowBook)
 
+	// Get all available author list
 	e.GET("/author", app.ShowAuthor)
-
+	// Get all category of books
 	e.GET("/category", app.ShowCategories)
 }
